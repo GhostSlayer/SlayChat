308868d1ba9ef925fbacef71b8576a7257468315
Slayer <aleksi.klen@gmail.com>
2020-11-19 20:39:47 +0200

    socket.io

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const app = express();
 const logger = require('./utils/logger');
 const morgan = require('morgan');
 const chalk = require('chalk');
+const uuid = require('uuid');
 
 const port = 3000 || 3001;
 
@@ -13,6 +14,23 @@ app.get('/', (req, res) =>{
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(port, () => {
-  logger.log('Listening on the port ' + port, { color: 'green', tags: ['Server'] })
-});
+server = app.listen(port, () => logger.log('Listening on the port ' + port, { color: 'green', tags: ['Server'] }));
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+  io.emit('chat message', 'A user has joined the chat');
+  logger.log('A user has joined the chat', { color: 'green', tags: ['Socket'] })
+
+  socket.on('disconnect', () => {
+    io.emit('chat message', 'A user has left the chat');
+    logger.log('A user has disconnected from the chat', { color: 'red', tags: ['Socket'] })
+  });
+
+  socket.on('chat message', (msg) => {
+    // block the message if there is nothing
+    if (!msg) return;
+
+    logger.log('Received a message! ' + msg , { color: 'green', tags: ['Socket'] })
+    io.emit('chat message', msg);
+  });
+})
